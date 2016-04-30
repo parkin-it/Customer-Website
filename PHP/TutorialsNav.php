0d5e141e677b55f268fb99ed24a046e908e7dbcf
parkin-it <natecheadle@gmail.com>
2016-04-29 17:04:33 -0700

    Migrated NavBar to PHP file

---
+++ PHP/TutorialsNav.php
@@ -0,0 +1,135 @@
+<?php
+echo'<!-- Nav -->
+				<nav id="nav">
+					<ul>
+						<li><a href="../index.html">Home</a></li>
+						<li>
+							<a href="../Services/Services.html">Your Services</a>
+							<ul>
+								<li><a href="../Services/DataManagment.html">Data Managment</a>
+                                    <ul>
+										<li><a href="../Services/DatabaseBackup.html">Database Backup Service</a></li>
+										<li><a href="../Services/DatabaseCloud.html">Database Cloud Service</a></li>
+                                        <li><a href="../Services/BigData.html">Big Data Cloud Service</a></li>
+                                        <li><a href="../Services/NoSQL.html">NoSQL Database Cloud Service</a></li>
+                                        <li><a href="../Services/BigDataPrep.html">Big Data Preperation Cloud Service</a></li>
+								    </ul>
+                                </li>
+                                <li><a href="../Services/ContentAndProcess.html">Content and Process</a>
+                                    <ul>
+										<li><a href="../Services/Documents.html">Documents Cloud Service</a></li>
+										<li><a href="../Services/Process.html">Process Cloud Service</a></li>
+                                        <li><a href="../Services/Sites.html">Sites Cloud Service</a></li>
+                                        <li><a href="../Services/SocialNetwork.html">Social Network Cloud Service</a></li>
+								    </ul>
+                                </li>
+								<li><a href="../Services/AppDevelopment.html">Application Development</a>
+                                    <ul>
+										<li><a href="../Services/Java.html">Java Cloud Service</a></li>
+                                        <li><a href="../Services/Mobile.html">Mobile Cloud Service</a></li>
+                                        <li><a href="../Services/Messaging.html">Messaging Cloud Service</a></li>
+                                        <li><a href="../Services/AppContainer.html">Application Container Cloud Service</a></li>
+                                        <li><a href="../Services/Developer.html">Developer Cloud Service</a></li>
+                                        <li><a href="../Services/AppBuilder.html">Applicaiton Builder Cloud Service</a></li>
+                                        <li><a href="../Services/APICatalog">API Catalog Cloud Service</a></li>
+								    </ul>
+                                </li>
+                                <li><a href="../Services/BusinessAnalytics.html">Business Analytics</a>
+                                    <ul>
+										<li><a href="../Services/BI.html">Business Intelligence Cloud Service</a></li>
+                                        <li><a href="../Services/BigDataDiscovery.html">Big Data Discovery Cloud Service</a></li>
+                                        <li><a href="../Services/DataVisualization.html">Data Visualization Cloud Service</a></li>
+                                        <li><a href="../Services/IOT.HTML">Internet of Things Cloud Service</a></li>
+								    </ul>
+                                </li>
+                                <li><a href="../Services/Integration.html">Integration</a>
+                                    <ul>
+										<li><a href="../Services/Integration.html">Integration Cloud Service</a></li>
+                                        <li><a href="../Services/SOA.html">SOA Cloud Service</a></li>
+                                        <li><a href="../Services/GoldenGate.html">GoldenGate Cloud Service</a></li>
+								    </ul>
+                                </li>
+                                <li><a href="../Services/Managment.html">IT Management</a>
+                                    <ul>
+										<li><a href="../Services/LogAnalytics.html">Log Analytics Cloud Service</a></li>
+                                        <li><a href="../Services/ITAnalytics.html">IT Analytics Cloud Service</a></li>
+                                        <li><a href="../Services/AppPerformanceMonitoring.html">Application Performance Monitoring Cloud Service</a></li>
+								    </ul>
+                                </li>
+								<li><a href="../Services/IaaS.html">Infrastructure as a Service</a>
+                                    <ul>
+										<li><a href="../Services/ComputeCloud.html">Compute Cloud Service</a></li>
+										<li><a href="../Services/StorageCloud.html">Storage Cloud Service</a></li>
+                                        <li><a href="../Services/NetworkCloud.html">Network Cloud Service</a></li>
+                                        <li><a href="../Services/CloudMachine.html">Oracle Cloud Machine</a></li>
+								    </ul>
+								</li>
+							</ul>
+						</li>
+						<li><a href="../AccountTeam.html">Account Team</a></li>
+                        <li>
+                            <a href="Tutorials.html">Tutorials</a>
+                            <ul>
+								<li><a href="DataManagment.html">Data Managment</a>
+                                    <ul>
+										<li><a href="DatabaseBackup.html">Database Backup Service</a></li>
+										<li><a href="DatabaseCloud.html">Database Cloud Service</a></li>
+                                        <li><a href="BigData.html">Big Data Cloud Service</a></li>
+                                        <li><a href="NoSQL.html">NoSQL Database Cloud Service</a></li>
+                                        <li><a href="BigDataPrep.html">Big Data Preperation Cloud Service</a></li>
+								    </ul>
+                                </li>
+                                <li><a href="ContentAndProcess.html">Content and Process</a>
+                                    <ul>
+										<li><a href="Documents.html">Documents Cloud Service</a></li>
+										<li><a href="Process.html">Process Cloud Service</a></li>
+                                        <li><a href="Sites.html">Sites Cloud Service</a></li>
+                                        <li><a href="SocialNetwork.html">Social Network Cloud Service</a></li>
+								    </ul>
+                                </li>
+								<li><a href="AppDevelopment.html">Application Development</a>
+                                    <ul>
+										<li><a href="Java.html">Java Cloud Service</a></li>
+                                        <li><a href="Mobile.html">Mobile Cloud Service</a></li>
+                                        <li><a href="Messaging.html">Messaging Cloud Service</a></li>
+                                        <li><a href="AppContainer.html">Application Container Cloud Service</a></li>
+                                        <li><a href="Developer.html">Developer Cloud Service</a></li>
+                                        <li><a href="AppBuilder.html">Applicaiton Builder Cloud Service</a></li>
+                                        <li><a href="APICatalog">API Catalog Cloud Service</a></li>
+								    </ul>
+                                </li>
+                                <li><a href="BusinessAnalytics.html">Business Analytics</a>
+                                    <ul>
+										<li><a href="BI.html">Business Intelligence Cloud Service</a></li>
+                                        <li><a href="BigDataDiscovery.html">Big Data Discovery Cloud Service</a></li>
+                                        <li><a href="DataVisualization.html">Data Visualization Cloud Service</a></li>
+                                        <li><a href="IOT.HTML">Internet of Things Cloud Service</a></li>
+								    </ul>
+                                </li>
+                                <li><a href="Integration.html">Integration</a>
+                                    <ul>
+										<li><a href="Integration.html">Integration Cloud Service</a></li>
+                                        <li><a href="SOA.html">SOA Cloud Service</a></li>
+                                        <li><a href="GoldenGate.html">GoldenGate Cloud Service</a></li>
+								    </ul>
+                                </li>
+                                <li><a href="Managment.html">IT Management</a>
+                                    <ul>
+										<li><a href="LogAnalytics.html">Log Analytics Cloud Service</a></li>
+                                        <li><a href="ITAnalytics.html">IT Analytics Cloud Service</a></li>
+                                        <li><a href="AppPerformanceMonitoring.html">Application Performance Monitoring Cloud Service</a></li>
+								    </ul>
+                                </li>
+								<li><a href="IaaS.html">Infrastructure as a Service</a>
+                                    <ul>
+										<li><a href="ComputeCloud.html">Compute Cloud Service</a></li>
+										<li><a href="StorageCloud.html">Storage Cloud Service</a></li>
+                                        <li><a href="NetworkCloud.html">Network Cloud Service</a></li>
+                                        <li><a href="CloudMachine.html">Oracle Cloud Machine</a></li>
+								    </ul>
+								</li>
+							</ul>
+                        </li>
+					</ul>
+				</nav>'
+?>
